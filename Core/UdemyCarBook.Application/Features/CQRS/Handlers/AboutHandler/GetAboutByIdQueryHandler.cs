7305016ee0ea6beb6422242bb212203da75d8a17
Efrun Evdi <efruncetkin@gmail.com>
2023-12-01 17:01:31 +0300

    16. Ders Sonu

---
+++ Core/UdemyCarBook.Application/Features/CQRS/Handlers/AboutHandler/GetAboutByIdQueryHandler.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UdemyCarBook.Application.Features.CQRS.Queries;
+using UdemyCarBook.Application.Features.CQRS.Results.AboutResults;
+using UdemyCarBook.Application.Interfaces;
+using UdemyCarBook.Domain.Entities;
+
+namespace UdemyCarBook.Application.Features.CQRS.Handlers.AboutHandler
+{
+    public class GetAboutByIdQueryHandler
+    {
+        private readonly IRepository<About> _repository;
+
+        public GetAboutByIdQueryHandler(IRepository<About> repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<GetAboutByIdQueryResult> Handle(GetAboutByIdQuery query)
+        {
+            var values = await _repository.GetByIdAsync(query.Id);
+
+            return new GetAboutByIdQueryResult
+            {
+                AboutID = values.AboutID,
+                Title = values.Title,
+                Description = values.Description,
+                ImageUrl = values.ImageUrl
+            };
+        }
+    }
+}
--- Presentation/UdemyCarBook.WebApi/Program.cs
+++ Presentation/UdemyCarBook.WebApi/Program.cs
@@ -1,6 +1,20 @@
+using UdemyCarBook.Application.Features.CQRS.Handlers.AboutHandler;
+using UdemyCarBook.Application.Interfaces;
+using UdemyCarBook.Persistence.Context;
+using UdemyCarBook.Persistence.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddScoped<CarBookContext>();
+
+builder.Services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
+
+builder.Services.AddScoped<GetAboutQueryHandler>();
+builder.Services.AddScoped<GetAboutByIdQueryHandler>();
+builder.Services.AddScoped<CreateAboutCommandHandler>();
+builder.Services.AddScoped<UpdateAboutCommandHandler>();
+builder.Services.AddScoped<RemoveAboutCommandHandler>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
