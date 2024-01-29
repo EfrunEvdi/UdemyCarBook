622f34f1a40d3ccc8d92cf21110431aacdefdb64
Efrun Evdi <efruncetkin@gmail.com>
2024-01-29 20:17:12 +0300

    41. Ders Sonu

---
+++ Core/UdemyCarBook.Application/Features/Mediator/Command/PricingCommands/CreatePricingCommand.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UdemyCarBook.Application.Features.Mediator.Command.PricingCommands
+{
+    public class CreatePricingCommand : IRequest
+    {
+        public int PricingID { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ Core/UdemyCarBook.Application/Features/Mediator/Handlers/SocialMediaHandlers/UpdateSocialMediaCommandHandler.cs
@@ -0,0 +1,33 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UdemyCarBook.Application.Features.Mediator.Command.SocialMediaCommands;
+using UdemyCarBook.Application.Interfaces;
+using UdemyCarBook.Domain.Entities;
+
+namespace UdemyCarBook.Application.Features.Mediator.Handlers.SocialMediaHandlers
+{
+    public class UpdateSocialMediaCommandHandler : IRequestHandler<UpdateSocialMediaCommand>
+    {
+        private readonly IRepository<SocialMedia> _repository;
+
+        public UpdateSocialMediaCommandHandler(IRepository<SocialMedia> repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task Handle(UpdateSocialMediaCommand request, CancellationToken cancellationToken)
+        {
+            var values = await _repository.GetByIdAsync(request.SocialMediaID);
+
+            values.Name = request.Name;
+            values.Icon = request.Icon;
+            values.Url = request.Url;
+
+            await _repository.UpdateAsync(values);
+        }
+    }
+}
