a80a8a09b8ec7d84100e0330ac9c33d24bd7484e
Efrun Evdi <efruncetkin@gmail.com>
2024-02-05 21:39:37 +0300

    52. Ders Sonu

---
+++ Frontends/UdemyCarBook.Web.UI/ViewComponents/AboutViewComponents/_BecomeADriverComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace UdemyCarBook.Web.UI.ViewComponents.AboutViewComponents
+{
+    public class _BecomeADriverComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
---
+++ Frontends/UdemyCarBook.Web.UI/ViewComponents/TestimonialViewComponents/_TestimonialComponentPartial.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace UdemyCarBook.Web.UI.ViewComponents.TestimonialViewComponents
+{
+    public class _TestimonialComponentPartial : ViewComponent
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _TestimonialComponentPartial(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7017/api/Testimonials");
+
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<>>(jsonData);
+                return View(values);
+            }
+
+            return View();
+        }
+    }
+}
