0743b0c52f0f25552fbedc5b14ccf040b0776c1a
Efrun Evdi <efruncetkin@gmail.com>
2024-02-07 20:35:25 +0300

    55. Ders Sonu

---
+++ Core/UdemyCarBook.Application/Features/Mediator/Handlers/TestimonialHandlers/UpdateTestimonialCommandHandler.cs
@@ -0,0 +1,34 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UdemyCarBook.Application.Features.Mediator.Command.TestimonialCommands;
+using UdemyCarBook.Application.Interfaces;
+using UdemyCarBook.Domain.Entities;
+
+namespace UdemyCarBook.Application.Features.Mediator.Handlers.TestimonialHandlers
+{
+    public class UpdateTestimonialCommandHandler : IRequestHandler<UpdateTestimonialCommand>
+    {
+        private readonly IRepository<Testimonial> _repository;
+
+        public UpdateTestimonialCommandHandler(IRepository<Testimonial> repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task Handle(UpdateTestimonialCommand request, CancellationToken cancellationToken)
+        {
+            var values = await _repository.GetByIdAsync(request.TestimonialID);
+
+            values.Title = request.Title;
+            values.Name = request.Name;
+            values.Comment = request.Comment;
+            values.ImageUrl = request.ImageUrl;
+
+            await _repository.UpdateAsync(values);
+        }
+    }
+}
--- Frontends/UdemyCarBook.Web.UI/Controllers/ServiceController.cs
+++ Frontends/UdemyCarBook.Web.UI/Controllers/ServiceController.cs
@@ -1,27 +1,27 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
-using UdemyCarBook.Dto.TestimonialDtos;
+using UdemyCarBook.Dto.ServiceDtos;
 
-namespace UdemyCarBook.Web.UI.ViewComponents.TestimonialViewComponents
+namespace UdemyCarBook.Web.UI.Controllers
 {
-    public class _TestimonialComponentPartial : ViewComponent
+    public class ServiceController : Controller
     {
         private readonly IHttpClientFactory _httpClientFactory;
 
-        public _TestimonialComponentPartial(IHttpClientFactory httpClientFactory)
+        public ServiceController(IHttpClientFactory httpClientFactory)
         {
             _httpClientFactory = httpClientFactory;
         }
 
-        public async Task<IViewComponentResult> InvokeAsync()
+        public async Task<IActionResult> Index()
         {
             var client = _httpClientFactory.CreateClient();
-            var responseMessage = await client.GetAsync("https://localhost:7017/api/Testimonials");
+            var responseMessage = await client.GetAsync("https://localhost:7017/api/Services");
 
             if (responseMessage.IsSuccessStatusCode)
             {
                 var jsonData = await responseMessage.Content.ReadAsStringAsync();
-                var values = JsonConvert.DeserializeObject<List<ResultTestimonialDto>>(jsonData);
+                var values = JsonConvert.DeserializeObject<List<ResultServiceDto>>(jsonData);
                 return View(values);
             }
 
